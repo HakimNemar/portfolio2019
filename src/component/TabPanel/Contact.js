20c86279f0413cb3b5dd10c0b121910d265105c1
hakim-nemar <hakim.nemar@epitech.eu>
2020-07-06 00:44:35 +0200

    css PanelContact

--- src/component/TabPanel/Contact.js
+++ src/component/TabPanel/Contact.js
@@ -1,55 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import emailjs from 'emailjs-com';
 
 function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [isReady, setIsReady] = useState(false);
+    const [form, setForm] = useState("");
+
     function sendEmail(e) {
         e.preventDefault();
+        let invalids = {};
 
-        emailjs.sendForm('gmail', 'template_kuMy2OpZ', e.target, 'user_5VwJPxWIJ4W9EYRUBTIdl').then((result) => {
-            console.log('good', result.text);
-        }, (error) => {
-            console.log('bad', error.text);
-        });
+        if (name == "") {
+            
+        }
+        // setForm(e.target);
+        // setIsReady(true);
     }
+    
+    useEffect(() => {
+        if (isReady) {
+            setIsReady(false);
+                emailjs.sendForm('gmail', 'template_kuMy2OpZ', form, 'user_5VwJPxWIJ4W9EYRUBTIdl').then((result) => {
+                    console.log('good', result.text);
+                }, (error) => {
+                    console.log('bad', error.text);
+                });
+        }
+    }, [isReady]);
 
     return (
         <>
-            <div className="container about m-5 pl-5">
+            <div className="container PanelContact m-5 pl-5">
                 <h1 className="title">CONTACT</h1>
                 <hr className="HR" />
-                {/* <form className="contact-form mt-4" onSubmit={sendEmail}>
-                    
-                    <label for="name">Name</label>
-                    <input type="text" name="name" />
-
-                    <label for="email">Email</label>
-                    <input type="email" name="email" />
-
-                    <label for="message">Message</label>
-                    <textarea name="message" />
-
-                    <input type="submit" value="Send" />
-                </form> */}
-
-                <form>
-                    <div>
-                        <div class="input-field col s12">
-                            <input id="password" type="text" class="validate" name="name" />
-                            <label for="name">Nom</label>
+                <div>
+                    <form onSubmit={sendEmail}>
+                        <div className="my-5">
+                            <div className="input-field ">
+                                <input id="password" type="text" className="validate" name="name"  onChange={(e) => setName(e.target.value)}/>
+                                <label htmlFor="name">Nom</label>
+                            </div>
+                            <div className="input-field">
+                                <input id="email" type="email" className="validate" name="email" onChange={(e) => setEmail(e.target.value)}/>
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div>
+                                <div className="input-field">
+                                    <textarea id="textarea1" type="text" className="materialize-textarea validate" name="message" onChange={(e) => setMessage(e.target.value)}/>
+                                    <label htmlFor="message">Message</label>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="input-field col s12">
-                        <input id="email" type="email" class="validate" name="email"/>
-                        <label for="email">Email</label>
-                    </div>
-                    <div>
-                        <div class="input-field col s12">
-                            <textarea id="textarea1" type="text" class="materialize-textarea" name="message"/>
-                            <label for="message">Message</label>
-                        </div>
-                    </div>
-                    {/* <input type="submit" value="Send" /> */}
-                </form>
+                        <input className="btn sendEmail" type="submit" value="Send" />
+                    </form>
+                </div>
             </div>
         </>
     )
